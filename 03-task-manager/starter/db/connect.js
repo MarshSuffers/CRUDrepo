6a58401d9b1673b086f22a7892de4d4f2c9b683f
Gabby <murkymarshmallow@gmail.com>
2026-08-17 12:17:20 -0600

    Create connect.js

---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://gabbytooele13_db_user:<test>@cluster0.b4hqems.mongodb.net/?appName=Cluster0'
+
+mongoose.connect(connectionString).then(()=>console.log('CONNECTED TO THE DB...')).catch((err) => console.log(err))
